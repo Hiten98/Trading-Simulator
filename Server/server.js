6f7843451f21aa0d178cd66a0c6e3bdc57fd1504
Hiten98 <hitenrathod98@gmail.com>
2018-04-21 15:46:51 -0400

    refresh token

--- Server/server.js
+++ Server/server.js
@@ -20,11 +20,78 @@ app.use(morgan('dev'));
 //set up port
 var port = (process.env.PORT || 9090 || 443);
 
+// set up min
+var min = 60;
+
+//bool to check if i need to find cycles
+var checkForCycles = false;
+
 //set up body parser for JSON
 const bodyParser = require('body-parser');
 
 //database
 var db = require('./database.js');
+
+//set up JSON of currencies:
+currencies = {};
+
+//set up a JSON of volatility
+volatility = {};
+
+function init() {
+  currencies["USD"] = 1;
+  currencies["EUR"] = 1.23;
+  currencies["JPY"] = 0.0093;
+  currencies["GBP"] = 1.40;
+  currencies["AUD"] = 0.77;
+  currencies["CAD"] = 0.78;
+  currencies["CHF"] = 1.03;
+  currencies["CNY"] = 0.16;
+  currencies["SEK"] = 0.12;
+  currencies["MXN"] = 0.054;
+  currencies["NZD"] = 0.72;
+  currencies["SGD"] = 0.76;
+  currencies["HKD"] = 0.13;
+  currencies["NOK"] = 0.13;
+  currencies["KRW"] = 0.00094;
+  currencies["TRY"] = 0.25;
+  currencies["INR"] = 0.015;
+  currencies["RUB"] = 0.016;
+  currencies["BRL"] = 0.29;
+  currencies["ZAR"] = 0.083;
+  currencies["DKK"] = 0.16;
+  currencies["PLN"] =0.29 ;
+  currencies["TWD"] = 0.034;
+  currencies["THB"] = 0.032;
+  currencies["MYR"] = 0.26;
+
+
+  volatility["USD"] = 0;
+  volatility["EUR"] = 0.7;
+  volatility["JPY"] = 1;
+  volatility["GBP"] = 0.85;
+  volatility["AUD"] = 0.71;
+  volatility["CAD"] = 0.04;
+  volatility["CHF"] = 0.56;
+  volatility["CNY"] = 0.25;
+  volatility["SEK"] = 0.78;
+  volatility["MXN"] = 0.68;
+  volatility["NZD"] = 0.12;
+  volatility["SGD"] = 0.90;
+  volatility["HKD"] = 0.20;
+  volatility["NOK"] = 0.41;
+  volatility["KRW"] = 0.36;
+  volatility["TRY"] = 0.91;
+  volatility["INR"] = 0.84;
+  volatility["RUB"] = 0.75;
+  volatility["BRL"] = 0.91;
+  volatility["ZAR"] = 0.83;
+  volatility["DKK"] = 0.16;
+  volatility["PLN"] =0.29 ;
+  volatility["TWD"] = 0.84;
+  volatility["THB"] = 0.52;
+  volatility["MYR"] = 0.90;
+}
 
 //setting up CORS
 //app.use(express.methodOverride());
@@ -49,13 +116,19 @@ app.use(allowCrossDomain);
 //set bodyParser
 app.use(bodyParser.json());
 
-var token2 = '';
-
 //default handler
 app.get('/', function(req, res) {
   console.log('In default / fn');
   res.send('Hello');
 })
+
+//jwt making function
+function makeJWT (payload) {
+  var token =  jwt.sign(payload, app.get('secret'), {
+    'expiresIn': 60*60
+  });
+  return token;
+}
 
 //login handler
 app.post('/LOGIN', function (req, res) {
@@ -68,10 +141,7 @@ app.post('/LOGIN', function (req, res) {
       const payload = {
         'email': email
       }
-      var token = jwt.sign(payload, app.get('secret'), {
-        'expiresIn': 60*60
-      });
-      token2 = token;
+      var token = makeJWT(payload);
       res.json({
         'status': true,
         'message': 'Successfully logged in',
@@ -85,7 +155,7 @@ app.post('/LOGIN', function (req, res) {
       });
     }
   });
-})
+});
 
 //register handler
 app.post('/REGISTER', function (req, res) {
@@ -123,6 +193,15 @@ app.post('/REFRESH-TOKEN', function (req, res) {
       else {
         console.log(decoded);
         req.decoded = decoded;
+        const payload = {
+          'email': decoded.email
+        }
+        var token = makeJWT(payload);
+        res.json({
+          'status': true,
+          'message': 'Successfully refreshed token',
+          'token': token
+        });
       }
     })
   }
@@ -134,6 +213,67 @@ app.post('/REFRESH-TOKEN', function (req, res) {
   }
 })
 
+//verify token
+app.post('/VERIFY-TOKEN', function (req, res) {
+  console.log(req.body);
+  var token = req.body.token;
+  if (token) {
+    jwt.verify(token, app.get('secret'), function(err, decoded) {
+      if(err) {
+        res.json({
+          'status': false,
+          'message': 'token could not be verified'
+        })
+      }
+      else {
+        console.log(decoded);
+        req.decoded = decoded;
+        var token = makeJWT(payload);
+        res.json({
+          'status': true,
+          'message': 'Token verified'
+        });
+      }
+    })
+  }
+  else {
+    res.status(404).json({
+      'status': false,
+      'message': 'token not found'
+    })
+  }
+})
+
+//route to update things:
+app.post('/UPDATE-THINGS', function (req, res) {
+  console.log(req.body);
+  min = req.body.min;
+  checkForCycles = req.body.checkForCycles;
+  res.json({
+    'status': true,
+    'min': min,
+    'checkForCycles': checkForCycles
+  })
+})
+
+//reset password
+
+//sleep for
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//async fn to update all values
+async function update() {
+  var i = 1;
+  while (true) {
+    console.log(i);
+    i++;
+    await sleep(1000*min);
+  }
+}
+
+
 //main fn
 app.listen(port, function() {
   console.log("server starts on port:");
@@ -141,6 +281,11 @@ app.listen(port, function() {
   console.log('connecting to sql database');
   db.connect();
   console.log('Successfully connected to sql database');
+  console.log('initialising values:');
+  console.log('done init');
+  console.log('start updating values');
+  update();
+  console.log('it should be updating now');
 })
 
 //error handler
